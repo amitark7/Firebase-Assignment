911ba0139cc6465e9c4fae0005a893770d4e7f1c
amitark7 <kumaramit21470@gmail.com>
2024-05-04 12:20:22 +0530

    setup for register

---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const RegisterPage = () => {
+  return (
+    <View className="bg-red-600 flex-1 items-center justify-center">
+      <Text>Hey This is Register Page</Text>
+    </View>
+  );
+};
+
+export default RegisterPage;
+
+// const styles = StyleSheet.create({});

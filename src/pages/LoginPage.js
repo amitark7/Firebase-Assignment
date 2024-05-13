1e4976de181d010cfb869155efb7a8451668c593
amitark7 <kumaramit21470@gmail.com>
2024-05-13 10:30:01 +0530

    remove extra code

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -6,17 +6,13 @@ import {
   TouchableOpacity,
   Platform,
 } from "react-native";
-import { FacebookAuthProvider, signInWithPopup } from "firebase/auth";
+import { signInWithPopup } from "firebase/auth";
 import {
   auth,
   fbProvider,
   googleProvider,
   twiiterProvider,
 } from "../firebase/firebaseConfig";
-// import auth from "@react-native-firebase/auth";
-// import { GoogleSignin } from "@react-native-google-signin/google-signin";
-import { AccessToken, LoginManager } from "react-native-fbsdk-next";
-import RNTwitterSignIn from "@react-native-twitter-signin/twitter-signin";
 import { useSelector, useDispatch } from "react-redux";
 import { ActivityIndicator } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
@@ -38,7 +34,10 @@ const LoginPage = ({ navigation }) => {
     const { isValid, errors } = validateForm(userData, true);
     if (isValid) {
       const result = await dispatch(userLogin(userData));
-      if (result.payload?.code === "auth/invalid-credential" || result.payload?.code === "auth/invalid-email") {
+      if (
+        result.payload?.code === "auth/invalid-credential" ||
+        result.payload?.code === "auth/invalid-email"
+      ) {
         setErrors({ ...errors, authError: "Email or Password is incorrect" });
       } else {
         navigation.replace("HomePage");
@@ -61,67 +60,9 @@ const LoginPage = ({ navigation }) => {
           phoneNumber: user.phoneNumber,
           pictureUrl: user.photoURL,
         };
-        try {
-          await dispatch(saveSocialAuthData(userSocialData));
-          navigation.navigate("HomePage");
-        } catch (error) {
-          console.log(error);
-        }
-      } else {
-        if (provider === fbProvider) {
-          await LoginManager.logInWithPermissions(["public_profile", "email"]);
-          const data = await AccessToken.getCurrentAccessToken();
-          console.log(data);
-          if (!data) {
-            return;
-          }
-          const facebookCredential = FacebookAuthProvider.credential(
-            data.accessToken
-          );
-          console.log(facebookCredential);
-          const response = await signInWithCredential(auth, facebookCredential);
-          console.log(response);
-        } else if (provider === googleProvider) {
-          if (Platform.OS === "android") {
-            GoogleSignin.configure({
-              webClientId:
-                "190676815337-kkkuhlkq95u98pf0ala29a7d5p22oqpr.apps.googleusercontent.com",
-            });
-          }
-          const { idToken } = await GoogleSignin.signIn();
-          const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-          const user_sign_in = auth().signInWithCredential(googleCredential);
-          user_sign_in
-            .then(({ user }) => {
-              console.log(user);
-              if (user.uid) {
-                const userSocialData = {
-                  userUID: user.uid,
-                  firstName: user.displayName.split(" ")[0],
-                  lastName: user.displayName.split(" ")[1],
-                  email: user.email,
-                  phoneNumber: user.phoneNumber,
-                  pictureUrl: user.photoURL,
-                };
-                dispatch(saveSocialAuthData(userSocialData));
-                navigation.replace("HomePage");
-              }
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        } else if (provider === twiiterProvider) {
-          await RNTwitterSignIn.init(
-            "wNxxKTZqeCRghIpFcxkZYH5oK",
-            "ujcoXj4XAkue9cDRzn0irQa2rAUUXSK8xu0eDZLPhPvCLBSShQ"
-          );
-          const { authToken, authTokenSecret } = await RNTwitterSignIn.logIn();
-          const twitterCredential = auth.TwitterAuthProvider.credential(
-            authToken,
-            authTokenSecret
-          );
-          const userSignIn = auth().signInWithCredential(twitterCredential);
-        }
+
+        await dispatch(saveSocialAuthData(userSocialData));
+        navigation.navigate("HomePage");
       }
     } catch (error) {
       console.log(error);
@@ -134,8 +75,9 @@ const LoginPage = ({ navigation }) => {
         <Text className="text-3xl font-semibold mb-6 text-center">Login</Text>
         <View className="w-full mb-6">
           <TextInput
-            className={`w-full rounded-md py-3 pl-2 outline-none border-2 ${errors.email ? "border-red-500" : "border-gray-300"
-              }`}
+            className={`w-full rounded-md py-3 pl-2 outline-none border-2 ${
+              errors.email ? "border-red-500" : "border-gray-300"
+            }`}
             placeholder="Email ID"
             value={userData.email}
             onChangeText={(text) => handleInputChange("email", text)}
@@ -144,8 +86,9 @@ const LoginPage = ({ navigation }) => {
         </View>
         <View className="w-full mb-6">
           <TextInput
-            className={`w-full rounded-md py-3 pl-2 outline-none border-2 ${errors.password ? "border-red-500" : "border-gray-300"
-              }`}
+            className={`w-full rounded-md py-3 pl-2 outline-none border-2 ${
+              errors.password ? "border-red-500" : "border-gray-300"
+            }`}
             placeholder="Password"
             secureTextEntry={true}
             value={userData.password}
@@ -155,8 +98,9 @@ const LoginPage = ({ navigation }) => {
           <ErrorComponent errorMessage={errors.authError} />
         </View>
         <TouchableOpacity
-          className={`w-full ${loading ? "bg-gray-400" : "bg-blue-500"
-            } py-2 text-xl rounded-md mb-4 font-semibold`}
+          className={`w-full ${
+            loading ? "bg-gray-400" : "bg-blue-500"
+          } py-2 text-xl rounded-md mb-4 font-semibold`}
           onPress={onLoginSubmit}
           disabled={loading}
         >

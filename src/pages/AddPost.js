7c3ce9145ff7a0b748569f72a476b74f4046ffae
amitark7 <kumaramit21470@gmail.com>
2024-05-16 16:13:11 +0530

    change UI view

--- src/pages/AddPost.js
+++ src/pages/AddPost.js
@@ -52,7 +52,7 @@ const AddPost = () => {
       title: "",
       description: "",
       slug: "",
-      picture: null,
+      picture: "",
     });
     setTaggedUser([]);
     richText.current.setContentHTML("");
@@ -94,11 +94,10 @@ const AddPost = () => {
   }, []);
 
   return (
-    <ScrollView>
+    <ScrollView className="bg-gray-300">
       <View className="flex-1 justify-center">
-        <View className="w-[90%] sm:w-[50%] lg:w-[35%] 2xl:w-[30%] bg-gray-300 mx-auto py-8 px-8 mb-10 rounded-lg shadow-lg">
-          <Text className="text-center text-2xl font-bold mb-2">Add Post</Text>
-          <View className="mb-2">
+        <View className="w-full sm:w-[50%] lg:w-[35%] 2xl:w-[30%]  mx-auto py-4 px-4 mb-10 rounded-lg shadow-lg">
+          <View className="mb-4">
             <TextInput
               className="border border-white bg-white rounded-md px-4 py-2 sm:py-3"
               placeholder="Enter Title"
@@ -114,7 +113,7 @@ const AddPost = () => {
             />
             <ErrorComponent errorMessage={errors.title} />
           </View>
-          <View className="mb-2">
+          <View className="mb-4">
             <TextInput
               className="border border-white bg-white rounded-md px-4 py-2 sm:py-3"
               placeholder="Your Slug"
@@ -122,7 +121,7 @@ const AddPost = () => {
               editable={false}
             />
           </View>
-          <View className="mb-2 h-[200px]">
+          <View className="mb-4 h-[150px]">
             <RichEditor
               ref={richText}
               useContainer={false}
@@ -133,32 +132,60 @@ const AddPost = () => {
                 });
                 setErrors({ ...errors, description: "" });
               }}
+              placeholder="Description"
             />
             <RichToolbar editor={richText} />
             <ErrorComponent errorMessage={errors.description} />
           </View>
-          <View className="mb-2">
-            <TouchableOpacity
-              className="py-3 px-4 bg-blue-500 rounded-md"
-              onPress={handleImageSelect}
-            >
-              <Text className="text-center text-white">Select Image</Text>
-            </TouchableOpacity>
+          <View className="mb-2 h-[120px] flex justify-center items-center bg-white border-gray-200 border rounded-lg">
+            {newPostData.picture ? (
+              <View className="relative w-[50%] mx-auto">
+                <Image
+                  source={{ uri: newPostData.picture }}
+                  className="h-[80px] w-full mt-2"
+                />
+                <TouchableOpacity
+                  className="absolute -right-1"
+                  onPress={() =>
+                    setNewPostData({ ...newPostData, picture: "" })
+                  }
+                >
+                  <FontAwesome5 name="times" size={16} />
+                </TouchableOpacity>
+              </View>
+            ) : (
+              <>
+                <TouchableOpacity
+                  className="py-3 px-4 rounded-md"
+                  onPress={handleImageSelect}
+                >
+                  <FontAwesome5 name="upload" size={20} />
+                </TouchableOpacity>
+              </>
+            )}
             <ErrorComponent errorMessage={errors.picture} />
           </View>
-          {newPostData.picture && (
-            <Image
-              source={{ uri: newPostData.picture }}
-              className="h-[100px] w-[100px] mt-2"
+          <View className="mt-1">
+            <DropDownPicker
+              open={open}
+              value={taggedUser}
+              items={users}
+              setOpen={setOpen}
+              setValue={setTaggedUser}
+              searchable={true}
+              placeholder="Search or choose a tag"
+              showBadgeDot={true}
+              multiple={true}
+              dropDownDirection="TOP"
             />
-          )}
+          </View>
           {taggedUser && (
-            <View className="flex flex-row flex-wrap">
+            <View className="flex flex-row flex-wrap mt-3">
               {taggedUser.map((user, index) => {
                 return (
                   <View
                     key={index}
-                    className="p-1 bg-blue-100 m-1 rounded relative"
+                    className="bg-white m-1  rounded relative p-2"
                   >
                     <Text className="text-black">{user}</Text>
                     <TouchableOpacity
@@ -172,24 +199,10 @@ const AddPost = () => {
               })}
             </View>
           )}
-          <View className="mt-1">
-            <DropDownPicker
-              open={open}
-              value={taggedUser}
-              items={users}
-              setOpen={setOpen}
-              setValue={setTaggedUser}
-              searchable={true}
-              placeholder="Tag User"
-              showBadgeDot={true}
-              multiple={true}
-              dropDownDirection="TOP"
-            />
-          </View>
           <TouchableOpacity
-            className={`p-3 ${
+            className={`p-3 w-[200px] mx-auto ${
               loading ? "bg-blue-300" : "bg-blue-500"
-            } rounded-md mt-4`}
+            } rounded-md mt-8`}
             onPress={submitNewPost}
             disabled={loading}
           >

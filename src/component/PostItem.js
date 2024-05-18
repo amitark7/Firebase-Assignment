29a8fba3a3c49d18a490fe2f5eebda5ff95f99a8
amitark7 <kumaramit21470@gmail.com>
2024-05-18 18:12:40 +0530

    implement all post page

---
+++ src/component/PostItem.js
@@ -0,0 +1,30 @@
+import { Image, Text, View, useWindowDimensions } from "react-native";
+import React from "react";
+import RenderHTML, { HTMLElementModel } from "react-native-render-html";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+const PostItem = ({ post }) => {
+  const { width } = useWindowDimensions();
+  return (
+    <View className="w-full rounded p-4 mb-4">
+      <Image
+        source={{ uri: post.picture }}
+        className="w-full h-40 mb-2 rounded"
+      />
+      <Text className="text-lg font-bold mb-2">{post.title}</Text>
+      <RenderHTML contentWidth={width} source={{ html: post.description }} />
+      <View className="flex-1 flex-row flex-wrap gap-1 mt-1">
+        {post.taggedUsers?.map((tag) => {
+          return (
+            <View className="p-1 bg-gray-300 rounded-lg w-max flex flex-row justify-center items-center gap-1">
+              <FontAwesome5 name="user" size={10} color="black" />
+              <Text className="">{tag}</Text>
+            </View>
+          );
+        })}
+      </View>
+    </View>
+  );
+};
+
+export default PostItem;
--- src/pages/AllPostPage.js
+++ src/pages/AllPostPage.js
@@ -1,10 +1,37 @@
-import { Text, View } from "react-native";
-import React from "react";
+import { FlatList, View, Text, ActivityIndicator } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPostList } from "../redux/reducer/postReducer";
+import PostItem from "../component/PostItem";
+import { useFocusEffect } from "@react-navigation/native";
 
 const AllPostPage = () => {
+  const { postList, loading } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+
+  useFocusEffect(
+    useCallback(() => {
+      dispatch(getPostList());
+    }, [dispatch])
+  );
+
   return (
     <View>
-      <Text>This is All Post Page</Text>
+      <FlatList
+        data={postList}
+        ItemSeparatorComponent={
+          <View className="h-[2px] bg-gray-300 mx-auto w-[91%]"></View>
+        }
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({ item }) => <PostItem post={item} />}
+        ListFooterComponent={
+          loading && (
+            <View className="flex-1 justify-center items-center">
+              <ActivityIndicator size={"large"} />
+            </View>
+          )
+        }
+      />
     </View>
   );
 };

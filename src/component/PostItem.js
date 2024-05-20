2c525aca1c6cf6c56113c3fa685f6f74fa15b59a
amitark7 <kumaramit21470@gmail.com>
2024-05-20 10:54:08 +0530

    add see more option

--- src/component/PostItem.js
+++ src/component/PostItem.js
@@ -1,19 +1,43 @@
-import { Image, Text, View, useWindowDimensions } from "react-native";
-import React from "react";
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  useWindowDimensions,
+} from "react-native";
+import React, { useState } from "react";
 import RenderHTML from "react-native-render-html";
 import { FontAwesome5 } from "@expo/vector-icons";
 
 const PostItem = ({ post }) => {
   const { width } = useWindowDimensions();
+  const [toggleShowMore, setToggleShowMore] = useState(false);
+
   return (
     <View className="w-[90%] mx-auto border-gray-400 rounded mb-4 border shadow-lg">
-      <Image
-        source={{ uri: post.picture }}
-        className="w-full h-52 mb-2"
-      />
+      <Image source={{ uri: post.picture }} className="w-full h-52 mb-2" />
       <View className="p-2">
         <Text className="text-lg font-bold mb-2">{post.title}</Text>
-        <RenderHTML contentWidth={width} source={{ html: post.description }} />
+        <RenderHTML
+          contentWidth={width}
+          source={{
+            html: `${
+              toggleShowMore
+                ? post.description
+                : post.description.substring(0, 200)
+            }`,
+          }}
+        />
+        {post.description.length > 200 && (
+          <TouchableOpacity
+            className="mt-2"
+            onPress={() => setToggleShowMore(!toggleShowMore)}
+          >
+            <Text className="text-sm">
+              {toggleShowMore ? "See Less" : "See More..."}
+            </Text>
+          </TouchableOpacity>
+        )}
         <View className="flex flex-row flex-wrap gap-1 mt-1">
           {post.taggedUsers?.map((tag, index) => {
             return (
